ef97a082c1492cb5a835aa0c11213a729a90f16f
pete-hulson <pete.hulson@noaa.gov>
2025-03-24 10:17:16 -0700

    set up first sim full run

--- dev/exp2_parallel.R
+++ dev/exp2_parallel.R
@@ -15,7 +15,7 @@ iters <- 1000
 ## sampling parameters ----
 
 # number of sampling units (e.g., hauls)
-su_num <- 500
+su_num <- 250
 
 # number of samples per sampling unit (e.g., ages/lengths)
 # vector to test differences in number of samples across sampling unit
@@ -27,13 +27,13 @@ p_su_samp <- c(0.9, 0.1)
 ## population unit parameters ----
 
 # number of population units sampled (e.g., number of schools/year classes)
-pu <- 5
+pu <- 25
 
 # number of population categories (e.g., ages or length bins)
 pc <- 15
 
 # CV around mean within pop'n unit (e.g., CV in mean age of school)
-pu_cv <- 0.2
+pu_cv <- 0.25
 
 # pop'n exponential decay (e.g., lnM, tied to inverse of number of pop'n categories so that pop'n = 0.01 at largest category)
 d <- log(0.01) / (1 - pc)
@@ -60,7 +60,7 @@ test2 <- function(sim_reps, d, pu, pc, su_num, su_samp, p_su_samp, iters){
   #start timer
   tictoc::tic()
   # set levels of cv
-  pu_cv_test <- c(0.05, 0.1, 0.25, 1)
+  pu_cv_test <- c(0.1, 0.25, 1, 100)
   # run simulation
   rr_cv <- purrr::map(1:sim_reps, ~purrr::map(1:length(pu_cv_test), ~rep_sim(d, pu, pc, pu_cv = pu_cv_test[.], su_num, su_samp, p_su_samp, iters)))
   # save & plot results
@@ -75,7 +75,7 @@ test3 <- function(sim_reps, d, pc, pu_cv, su_num, su_samp, p_su_samp, iters){
   #start timer
   tictoc::tic()
   # set numbers of pop'n units
-  npu_test <- c(5, 10, 25, 100)
+  npu_test <- c(10, 25, 100, 250)
   # run simulation
   rr_npu <- purrr::map(1:sim_reps, ~purrr::map(1:length(npu_test), ~rep_sim(d, pu = npu_test[.], pc, pu_cv, su_num, su_samp, p_su_samp, iters)))
   # save & plot results
@@ -104,8 +104,8 @@ test4 <- function(sim_reps, d, pu, pu_cv, su_num, su_samp, p_su_samp, iters){
 test5 <- function(sim_reps, d, pu, pc, pu_cv, su_samp, p_su_samp, iters){
   #start timer
   tictoc::tic()
-  # set numbers of categories
-  su_test <- c(50, 100, 250, 500)
+  # set number of sampling units
+  su_test <- c(100, 250, 500, 1000)
   # run simulation
   rr_su <- purrr::map(1:sim_reps, ~purrr::map(1:length(su_test), ~rep_sim(d, pu, pc, pu_cv, su_num = su_test[.], su_samp, p_su_samp, iters)))
   # save & plot results
@@ -116,15 +116,15 @@ test5 <- function(sim_reps, d, pu, pc, pu_cv, su_samp, p_su_samp, iters){
 }
 
 ## test sample size within sampling units ----
-test6 <- function(sim_reps, d, pu, pc, pu_cv, su_num, iters){
+test6 <- function(sim_reps, d, pu, pc, pu_cv, su_num, iters, plot_name){
   #start timer
   tictoc::tic()
-  # set numbers of categories
+  # set sample size within sampling units
   samp_test <- c(20, 50, 100, 250)
   # run simulation
   rr_samp <- purrr::map(1:sim_reps, ~purrr::map(1:length(samp_test), ~rep_sim(d, pu, pc, pu_cv, su_num, su_samp = c(samp_test[.], 10), p_su_samp = c(1, 0), iters)))
   # save & plot results
-  plot_sim(rr_samp, 'samp', samp_test, "N_Samp")
+  plot_sim(rr_samp, plot_name, samp_test, "N_Samp")
   # end timer
   runtime <- tictoc::toc()
   return(runtime)
@@ -133,7 +133,7 @@ test6 <- function(sim_reps, d, pu, pc, pu_cv, su_num, iters){
 ## run tests in parallel ----
 
 # number of simulation replicates for testing iss axes of influence
-sim_reps <- 1000
+sim_reps <- 10
 
 # set up parallel fcn
 run_tests <- function(sim_reps, d, pu, pc, pu_cv, su_num, su_samp, p_su_samp, iters){
@@ -142,13 +142,15 @@ run_tests <- function(sim_reps, d, pu, pc, pu_cv, su_num, su_samp, p_su_samp, it
   runtime_npu %<-% test3(sim_reps, d, pc, pu_cv, su_num, su_samp, p_su_samp, iters) %seed% TRUE
   runtime_cat %<-% test4(sim_reps, d, pu, pu_cv, su_num, su_samp, p_su_samp, iters) %seed% TRUE
   runtime_su %<-% test5(sim_reps, d, pu, pc, pu_cv, su_samp, p_su_samp, iters) %seed% TRUE
-  runtime_samp %<-% test6(sim_reps, d, pu, pc, pu_cv, su_num, iters) %seed% TRUE
+  runtime_samp_h250 %<-% test6(sim_reps, d, pu, pc, pu_cv, su_num, iters, 'samp_h250') %seed% TRUE
+  runtime_samp_h500 %<-% test6(sim_reps, d, pu, pc, pu_cv, su_num = 500, iters, 'samp_h500') %seed% TRUE
   runtimes <- c((runtime_exp$toc - runtime_exp$tic),
                 (runtime_cv$toc - runtime_cv$tic),
                 (runtime_npu$toc - runtime_npu$tic),
                 (runtime_cat$toc - runtime_cat$tic),
                 (runtime_su$toc - runtime_su$tic),
-                (runtime_samp$toc - runtime_samp$tic))
+                (runtime_samp_h250$toc - runtime_samp_h250$tic),
+                (runtime_samp_h500$toc - runtime_samp_h500$tic))
   return(runtimes)
 }
 
