cd52e25df1638173169f2fe0f959484e66ce5316
pete-hulson <pete.hulson@noaa.gov>
2025-03-27 10:52:37 -0800

    start notes

---
+++ docs/notes.R
@@ -0,0 +1,24 @@
+# exp 1 notes: ----
+
+# so long as the schools are represented across the haul samples in proportion to their relative abundance, 
+# the mean across iterations in unbiased, whether weighted or not, and regardless of selectivity shape
+#
+# you don't need to weight by haul's catch even if the number of samples within a haul aren't proportional to catch
+#
+# for any given iteration, the composition resulting from weighted or unweighted comps can be different.
+#
+# the uncertainty around weighted comps is larger than unweighted comps, for all selectivity 
+
+# exp 2 notes: ----
+#
+# iss for both expansion types < nss (horizontal line), simulation framework is able to produce overdispersion
+# will not show nss line in plots unless used to explain results
+#
+# unweighted iss > wtd iss, wtd iss takes into account additional source of uncertainty in sampling unit total/catch and 
+# potential for differing sample size
+#
+# no significant difference in iss across selectivity shapes, only difference was for multimodal, where iss for uniform
+# selex slightly larger than for other selex shapes, will integrate results across selectivity shapes in following results
+#
+# for both unweighted and weighted expansion types, pop'n unit structure influences magnitude of iss,
+# uncertainty in comps with recruitment pulse > multimodel > unimodal
